2676a502830e15cfa1d58ae7d7b23043ee679c9d
norman6464 <resjimkalto89890@emaiol.com>
2025-09-25 15:33:46 +0900

    x

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export function MainContent() {
+  return (
+    <main>
+      <section>
+        <h2>本教材について</h2>
+        <p>本教材では、Reactの基本的な文法や概念を実践的に学びます。</p>
+        <p>本教材では、Reactの基本的な文法や概念を実践的に学びます。</p>
+        <p>本教材では、Reactの基本的な文法や概念を実践的に学びます。</p>
+      </section>
+    </main>
+  );
+}
