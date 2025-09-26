317db9aa1101569b85cbaf87960e99595e1aa034
mayukasahara <fca2306070009@edu.fca.ac.jp>
2025-09-26 09:41:01 +0900

    event

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
 export function Header({ siteTitle = 'サイト名未設定' }) {
+
+  // ボタンクリック時に処理するイベントハンドラ
+  const handleClick = () => {
+    alert('当社では、プログラミング学習者向けにReacctを活用した実践的な学習教材を提供しています。');
+  }
+
   return (
     <header>
       <nav>
@@ -9,6 +15,7 @@ export function Header({ siteTitle = 'サイト名未設定' }) {
           <li>
             <a href="#">HOME</a>
           </li>
+          <li><a href='#' onClick={handleClick}>会社概要</a></li>
         </ul>
       </nav>
     </header>
